4ac55d52e5d6b812aabc8f70ca5bcff0b649ddba
salvo <salvo.cuccurullo@gmailcom>
2018-09-14 19:29:40 +0200

    adding support ajax call

--- app/movies-list/movies-list.component.js
+++ app/movies-list/movies-list.component.js
@@ -1,28 +1,38 @@
 // Register `moviesList` component, along with its associated controller and template
 
 angular.
-  module('moviesList').
-  component('moviesList', {
-    templateUrl: 'movies-list/movies-list.template.html',
-    controller: function MoviesListController() {
-      this.movies = [
-        {
-          name: 'Kobra Kai',
-          media: 'YouTube Premium'
-        },
-        {
-          name: 'Atypical season 2',
-          media: 'Netflix'
-        }, 
-        {
-          name: 'Mozart in the Jungle',
-          media: 'Amazon Video'
-        },
-        {
-          name: 'El Autor',
-          media: 'Netflix'
-        }
-      ];
-      this.orderProp = 'name';
-    }
-  });
+	module('moviesList').
+	component('moviesList', {
+		templateUrl: 'movies-list/movies-list.template.html',
+		controller: function MoviesListController($http) {
+
+			var self = this;
+			self.orderProp = 'title';
+
+			/*
+			this.movies = [
+				{
+				name: 'Kobra Kai',
+				media: 'YouTube Premium'
+				},
+				{
+				name: 'Atypical season 2',
+				media: 'Netflix'
+				}, 
+				{
+				name: 'Mozart in the Jungle',
+				media: 'Amazon Video'
+				},
+				{
+				name: 'El Autor',
+				media: 'Netflix'
+				}
+			];
+			*/
+
+			$http.get('http://localhost:8888/getTvShows').then(function(response) {
+				x = JSON.parse(response.data.payload);
+				self.movies = x.tvshows;
+			});
+		}
+});
--- app/movies-list/movies-list.component.spec.js
+++ app/movies-list/movies-list.component.spec.js
@@ -7,23 +7,33 @@ describe('moviesList', function() {
 	// Test the controller
 	describe('MoviesListController', function() {
 		
-		var ctrl;
+		var $httpBackend, ctrl;
 		
-		beforeEach(inject(function($componentController){
+		// The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+		// This allows us to inject a service and assign it to a variable with the same name
+		// as the service while avoiding a name conflict.
+		
+		beforeEach(inject(function($componentController, _$httpBackend_){
+			$httpBackend = _$httpBackend_;
+			$httpBackend.expectGET('http://localhost:8888/getTvShows').respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 			ctrl = $componentController("moviesList");
 		}));
 
-		it('should create a `movies` model with 4 movies', inject(function($componentController) {
+		it('should create a `movies` model with 47 movies', inject(function($componentController) {
 			var ctrl = $componentController('moviesList');
-			expect(ctrl.movies.length).toBe(4);
+			//expect(ctrl.movies).toBeUndefined();
+			//$httpBackend.flush();
+			//expect(ctrl.movies.length).toBe(47);
+			expect(ctrl.movies).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 		}));
 	
+	/*
 		it('should set a default value for the `orderProp` model', function() {
-			expect(ctrl.orderProp).toBe('name');
+			expect(ctrl.orderProp).toBe('title');
 		});
+	*/
 
-
-});
+	});
 
 });
 
